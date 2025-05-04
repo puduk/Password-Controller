52188c9284fb546522fc7f9b012f9ea00e57aebd
puduk <demirmert986@gmail.com>
2025-05-04 23:07:51 +0200

    Add data.cpp

---
+++ Password/data.cpp
@@ -0,0 +1,7 @@
+#include "data.hpp"
+
+data::data() {
+    company_address = new std::string("90210 Beverly Hills, Santa Monica Blvd, Los Angeles, CA");
+    email = new std::string("company00.mail.com");
+    phone_number = new std::string("202 - 222 - 220");
+}
