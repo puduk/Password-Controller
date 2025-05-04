86742238261dd10ae0923ac93be2cd5b77787627
puduk <demirmert986@gmail.com>
2025-05-04 23:08:08 +0200

    Add data.hpp

---
+++ Password/data.hpp
@@ -0,0 +1,35 @@
+#ifndef DATA_H
+#define DATA_H
+#include <iostream>
+
+class data {
+  public:
+
+    int* first_page_choice = new int;
+    int* help_choice = new int;
+    int* user_rate = new int;
+
+    std::string* username = new std::string;
+    std::string* password = new std::string;
+
+    std::string* phone_number ;
+    std::string* email ;
+    std::string* company_address ;
+
+    std::string* user_phone_number = new std::string;
+
+
+
+
+    data();
+    ~data(){
+      delete first_page_choice;
+      delete help_choice;
+      delete username;
+      delete password;
+    };
+};
+
+
+#endif
+
